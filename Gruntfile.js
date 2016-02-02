ce27bcb50c8af7aca20d2cd5aa04db92028782e3
Bruce McElroy <bruce.mcelroy@Gmail.com>
2016-02-02 05:15:20 -0500

    fixed routing

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function (grunt) {
         },
         watch: {
             sass: {
-                files: ['client/scss/*.scss'],
+                files: ['client/scss/**/*.scss'],
                 tasks: ['sass'],
                 options: {
                     spawn: false,
