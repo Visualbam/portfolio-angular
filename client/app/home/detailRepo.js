5838f36c7dfe008542b1a1261308e97143ac28a5
Bruce McElroy <bruce.mcelroy@Gmail.com>
2016-02-04 06:58:55 -0500

    house keeping

--- client/app/home/detailRepo.js
+++ client/app/home/detailRepo.js
@@ -15,22 +15,22 @@
             {
                 title: 'Design',
                 content: 'Understanding core design principles is key to expanding your capabilities. My degree is in design but I consider it a skillset in my developer toolbox.',
-                image: 'public/img/design.png'
+                image: './public/img/design.png'
             },
             {
                 title: 'Development',
                 content: 'Developing has become a passion of mine. I keep up with the latest frameworks and technologies to keep a proffesional advantage in my work.',
-                image: 'public/img/develop.png'
+                image: './public/img/develop.png'
             },
             {
                 title: 'UI/UX',
                 content: 'I create engaging mobile, tablet and desktop experiences. User experience is an essential aspect of any website or application.',
-                image: 'public/img/userexp.png'
+                image: './public/img/userexp.png'
             },
             {
                 title: 'Batman',
                 content: 'Always be yourself, unless you can be Batman. Then always be Batman. I get some of my best work done in the night.',
-                image: 'public/img/batman.png'
+                image: './public/img/batman.png'
             }
         ];
 
