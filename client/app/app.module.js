3ad71554212ea58df8d0bbba2bc48946d0b1d89c
Bruce McElroy <bruce.mcelroy@Gmail.com>
2016-02-04 13:14:04 -0500

    removed angular moment

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('app', ['ngRoute', 'angularMoment'])
+        .module('app', ['ngRoute'])
         .config(routeConfig);
 
     routeConfig.$inject = ['$routeProvider'];
