630de81aa2da8249ce9e74bbc42344f9aa8267c4
Bruce McElroy <bruce.mcelroy@Gmail.com>
2016-02-13 23:07:23 -0500

    added lazy loading

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -2,12 +2,20 @@
     'use strict';
 
     angular
-        .module('app', ['ngRoute', 'angularMoment', 'ui.bootstrap'])
+        .module('app', ['ngRoute', 'angularMoment', 'ui.bootstrap', 'angularLazyImg'])
         .config(routeConfig);
 
-    routeConfig.$inject = ['$routeProvider'];
+    routeConfig.$inject = ['$routeProvider', 'lazyImgConfigProvider'];
 
-    function routeConfig($routeProvider) {
+    function routeConfig($routeProvider, lazyImgConfigProvider) {
+        lazyImgConfigProvider.setOptions({
+            offset: 100, // how early you want to load image (default = 100)
+            errorClass: 'error', // in case of loading image failure what class should be added (default = null)
+            successClass: 'success', // in case of loading image success what class should be added (default = null)
+            onError: function(image){}, // function fired on loading error
+            onSuccess: function(image){} // function fired on loading success
+        });
+
         $routeProvider
             .when('/', {
                 title: 'Home',
